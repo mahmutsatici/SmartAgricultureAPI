5fa2aaf648b3074a0d0e93813897ed13913c10d7
mahmutsatici <st_falcon@outlook.com>
2025-01-02 12:34:05 +0300

    Model ve Connection String Oluşturuldu

---
+++ SmartAgricultureAPI/Data/AppDBContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartAgricultureAPI.Models;
+
+namespace SmartAgricultureAPI.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) { }
+
+        // DbSet'ler
+        public DbSet<Plant> Plants { get; set; }
+        public DbSet<PlantMeasurement> PlantMeasurements { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Plant - PlantMeasurement ilişkisi
+            modelBuilder.Entity<Plant>()
+                .HasMany(p => p.Measurements)
+                .WithOne(m => m.Plant)
+                .HasForeignKey(m => m.PlantId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
